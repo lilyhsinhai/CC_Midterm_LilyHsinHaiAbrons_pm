0a2fbde10b2330020a3ab1eb28d8f5e47ce5938b
lilyhsinhai <lha5017@nyu.edu>
2022-10-30 12:16:18 -0400

    shapes move at different speeds

--- README.md
+++ README.md
@@ -3,16 +3,32 @@
 
 
 **To Do**
-- [ ] make square (bright orange)
-	- [ ] make curved line for the path of the square (similar color)
-	- [ ] put the curved line in a class with variables for the vertices and pull points
-	- [ ] arrange square to be on the line
-	- [ ] get the curve to be continuous across the whole sketch
-	- [ ] get square to follow the path of the line
-- [ ] make circle (green)
-	- [ ] make curved line (different from square line) for the path of the circle (similar color) using my class
-	- [ ] arrange circle to be on the line
-	- [ ] get circle to follow the path of the line
+- [x] make square (bright orange)
+	- [x] make curved line for the path of the square (similar color)
+	- [x] put the curved line in a class with variables for the vertices and pull points
+	- [x] arrange square to be on the line
+	- [x] get the curve to be continuous across the whole sketch
+	- [x] get square to follow the path of the line
+- [x] make circle (green)
+	- [x] make curved line (different from square line) for the path of the circle (similar color) using my class
+	- [x] arrange circle to be on the line
+	- [x] get circle to follow the path of the line
+- [x] make triangle (blue)
+	- [x] make curved line (different from square and circel lines) for the path of the triangle (similar color) using my class
+	- [x] arrange triangle to be on the line
+	- [x] get triangle to follow the path of the line
+
+- [x] make shapes bounce back and forth on their lines
+- [ ] make it 3D
+	- [ ] make shapes 3D
+	- [ ] make wires 3D
+- [ ] use the z plane to make the wires "come at u"
+- [x] click on space bar to pause and unpause the shapes
+- [ ] click on shapes with mouse to change shape and randomize color
+- [ ] adjust speed
+- [ ] drag mouse to adjust wires
+- [x] have shapes move at different speeds on different wires
+- [ ] multiple shapes per wire (different speeds)
 
 
 **Must Haves**
--- childish.js
+++ childish.js
@@ -1,7 +1,7 @@
 let orangeWire;
 let greenWire;
 let blueWire;
-let pos = 0.0;
+let pause = false;
 
 function setup(){
   createCanvas(800,800);
@@ -9,35 +9,35 @@ function setup(){
 
   orangeWire = new Wire({x:0, y:450}, {x:100, y:600}, {x:200, y:150}, {x:350, y:400},
                         {x: 500, y:625}, {x:550, y:150}, {x:800,y:350}, color(255,123,22));
+  orangeWire.addShape(0.3, 0.005, color(255,95,0), "square");
 
   greenWire = new Wire({x:0, y:250}, {x:200, y:800}, {x:300, y:750}, {x:450, y:300},
                        {x: 550, y:0}, {x:650, y:500}, {x:800,y:450}, color(56,181,77));
+  greenWire.addShape(0.9, -0.008, color(56,148,56), "circle");
 
   blueWire = new Wire({x:0, y:50}, {x:150, y:0}, {x:50, y:400}, {x:400, y:200},
-                      {x: 700, y:40}, {x:300, y:600}, {x:800,y:700}, color(35,101,255));
+                      {x: 700, y:40}, {x:300, y:600}, {x:800, y:700}, color(35,101,255));
+  blueWire.addShape(0.7, 0.002, color(0,64,255), "triangle");
   
 }
 
 function draw(){
   background(255,241,196);
 
+//TO DO: condense this
+
   orangeWire.display();
-  orangeWire.drawShape(pos, color(255,95,0), "square");
+  orangeWire.move();
+  orangeWire.drawShape();
 
   greenWire.display();
-  greenWire.drawShape(pos, color(56,148,56), "circle");
+  greenWire.move();
+  greenWire.drawShape();
 
   blueWire.display();
-  blueWire.drawShape(pos,color(0,64,255), "triangle");
-
-  pos += 0.005;
-
-  if(pos >= 1){
-    pos = 0;
-  }
-
-  //console.log(pos)
-
+  blueWire.move();
+  blueWire.drawShape();
+  
 }
 
 class Wire{
@@ -55,6 +55,14 @@ class Wire{
 
   }
 
+  addShape(pos, speed, shapeColor, shapeType){
+    this.pos_ = pos;
+    this.speed_ = speed;
+    this.shapeColor_ = shapeColor;
+    this.shapeType_ = shapeType;
+
+  }
+
   display(){
 
     stroke(this.wireColor_);
@@ -69,30 +77,40 @@ class Wire{
 
   }
 
-  drawShape(pos, shapeColor, shapeType){
+  move(){
+    if(pause == false){
+      this.pos_ += this.speed_
+      if(this.pos_ >= 1|| this.pos_ <= 0.0){
+        this.speed_ = -this.speed_;
+      }
+    }
+
+  }
+
+  drawShape(){
     
     push();
     noStroke();
-    fill(shapeColor);
+    fill(this.shapeColor_);
     rectMode(CENTER);
     let x;
     let y;
-    if(pos <= 0.5){
-      x = bezierPoint(this.anchor1_.x, this.pull1_.x, this.pull2_.x, this.anchor2_.x,pos*2);
-      y = bezierPoint(this.anchor1_.y, this.pull1_.y,this.pull2_.y,this.anchor2_.y,pos*2);
+    if(this.pos_ <= 0.5){
+      x = bezierPoint(this.anchor1_.x, this.pull1_.x, this.pull2_.x, this.anchor2_.x,this.pos_*2);
+      y = bezierPoint(this.anchor1_.y, this.pull1_.y,this.pull2_.y,this.anchor2_.y,this.pos_*2);
     } else{
-      x = bezierPoint(this.anchor2_.x, this.pull3_.x, this.pull4_.x, this.anchor3_.x,(pos*2)-1);
-      y = bezierPoint(this.anchor2_.y, this.pull3_.y,this.pull4_.y,this.anchor3_.y,(pos*2)-1);
+      x = bezierPoint(this.anchor2_.x, this.pull3_.x, this.pull4_.x, this.anchor3_.x,(this.pos_*2)-1);
+      y = bezierPoint(this.anchor2_.y, this.pull3_.y,this.pull4_.y,this.anchor3_.y,(this.pos_*2)-1);
     }
     translate(x,y);
-    rotate(pos*PI*2);
-    if(shapeType == "square"){
+    rotate(this.pos_*PI*2);
+    if(this.shapeType_ == "square"){
       rect(0,0,25,25);
     }
-    if(shapeType == "circle"){
+    if(this.shapeType_ == "circle"){
       ellipse(0,0,35,25);
     }
-    if(shapeType == "triangle"){
+    if(this.shapeType_ == "triangle"){
       triangle(-15, 15, 0, -15, 15, 15);
     }
     pop();
@@ -101,5 +119,24 @@ class Wire{
 
 }
 
+function keyPressed(){
 
-  
+  /*if(key == "-"){
+    speed = speed*0.3;
+    formerSpeed = speed;
+  }
+  if(key == "="){
+    speed = speed*2;
+    formerSpeed = speed;
+  }*/
+  if(key == " "){
+    if(pause == true){
+      pause = false;
+    }else{
+      pause = true;
+    }
+  }
+}
+
+
+
