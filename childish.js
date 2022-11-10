d66ae2d2a94e22569010a43586421523f608e890
lilyhsinhai <lha5017@nyu.edu>
2022-11-09 19:06:43 -0500

    FInal Draft

--- childish.js
+++ childish.js
@@ -1,8 +1,10 @@
+//Lily Hsin-Hai Abrons - Midterm CC2pm F22
+
 new p5(); //https://github.com/processing/p5.js/wiki/p5.js-overview#why-cant-i-assign-variables-using-p5-functions-and-variables-before-setup
 
 let scenePause = false;
 
-let scene = 2;
+let scene = 1;
 
 const lowerBalls = [];
 
@@ -23,31 +25,31 @@ function setup(){
   let purpleWire;
   let redWire;
   let yellowWire;
-  let randomPalette = shuffle(palette);
+  let shufflePalette = shuffle(palette);
 
   orangeWire = new Wire({x:0, y:450}, {x:100, y:600}, {x:200, y:150}, {x:350, y:400},
                         {x: 500, y:625}, {x:550, y:150}, {x:800,y:350}, color(255,123,22));
-  orangeWire.addShape(0.3, 0.005, randomPalette.pop(), "square");
+  orangeWire.addShape(0.3, 0.005, shufflePalette.pop(), "square");
 
   greenWire = new Wire({x:0, y:250}, {x:200, y:800}, {x:300, y:750}, {x:450, y:300},
                        {x: 550, y:0}, {x:650, y:500}, {x:800,y:450}, color(56,181,77));
-  greenWire.addShape(0.9, -0.008, randomPalette.pop(), "circle");
+  greenWire.addShape(0.9, -0.008, shufflePalette.pop(), "circle");
 
   blueWire = new Wire({x:0, y:50}, {x:150, y:0}, {x:50, y:400}, {x:400, y:200},
                       {x: 700, y:40}, {x:300, y:600}, {x:800, y:700}, color(35,101,255));
-  blueWire.addShape(0.7, 0.003, randomPalette.pop(), "triangle");
+  blueWire.addShape(0.7, 0.003, shufflePalette.pop(), "triangle");
 
   purpleWire = new Wire({x:0, y:650}, {x:150, y:200}, {x:50, y:600}, {x:400, y:400},
                         {x: 800, y:200}, {x:200, y:800}, {x:800, y:650}, color(166,1,255));
-  purpleWire.addShape(0.7, -0.006, randomPalette.pop(), "square");
+  purpleWire.addShape(0.7, -0.006, shufflePalette.pop(), "square");
 
   redWire = new Wire({x:0, y:550}, {x:300, y:800}, {x:450, y:800}, {x:380, y:650},
                      {x: 250, y:400}, {x:200, y:800}, {x:800, y:750}, color(254,14,0));
-  redWire.addShape(0.8, -0.007, randomPalette.pop(), "triangle");
+  redWire.addShape(0.8, -0.007, shufflePalette.pop(), "triangle");
 
   yellowWire = new Wire({x:0, y:350}, {x:300, y:325}, {x:400, y:100}, {x:500, y:150},
                       {x: 700, y:250}, {x:600, y:0}, {x:800, y:150}, color(255,223,51));
-  yellowWire.addShape(0.4, 0.009, randomPalette.pop(), "circle");
+  yellowWire.addShape(0.4, 0.009, shufflePalette.pop(), "circle");
   
   wires.push(orangeWire, greenWire, blueWire, purpleWire, redWire, yellowWire);
 
@@ -89,7 +91,7 @@ class Ball{
     this.ballColor = ballColor;
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
-    this.deceleration = createVector (0.8, 0.8);
+    this.deceleration = 0.8;
 
   }
 
@@ -110,13 +112,11 @@ class Ball{
 
   ballClicked(){
 
-    const divisor = random(2000, 8000);
     var d = dist(mouseX, mouseY, this.position.x, this.position.y);
-      if(d < 200){
-        this.velocity = createVector(random(-30,30), random(-30,30));
+    if(d < 200){
+      this.velocity = createVector(random(-30,30), random(-30,30));
     }
   }
-
 }
 
 class Wire{
@@ -164,7 +164,7 @@ class Wire{
     if(this.pos_ <= 0.5){
       x = bezierPoint(this.anchor1_.x, this.pull1_.x, this.pull2_.x, this.anchor2_.x,this.pos_*2);
       y = bezierPoint(this.anchor1_.y, this.pull1_.y,this.pull2_.y,this.anchor2_.y,this.pos_*2);
-    } else{
+    }else{
       x = bezierPoint(this.anchor2_.x, this.pull3_.x, this.pull4_.x, this.anchor3_.x,(this.pos_*2)-1);
       y = bezierPoint(this.anchor2_.y, this.pull3_.y,this.pull4_.y,this.anchor3_.y,(this.pos_*2)-1);
     }
@@ -174,11 +174,10 @@ class Wire{
 
     if(this.pause_ == false){
       this.pos_ += this.speed_
-      if(this.pos_ >= 1|| this.pos_ <= 0.0){
-        this.speed_ = -this.speed_;
-      }
+     if(this.pos_ >= 1|| this.pos_ <= 0.0){
+       this.speed_ = -this.speed_;
+     }
     }
-
   }
 
   drawShape(){
@@ -200,7 +199,6 @@ class Wire{
       triangle(-15, 15, 0, -15, 15, 15);
     }
     pop();
-
   }
 
   faster(){
@@ -213,21 +211,17 @@ class Wire{
 
   shapeClicked(){
     var d = dist(mouseX, mouseY, this.x_, this.y_);
-      if(d < 30){
-        this.shapeColor_ = random(palette);
-      }
+    if(d < 30){
+      this.shapeColor_ = random(palette);
+    }
   }
 
   wireClicked(){
     var d = dist(mouseX, mouseY, this.anchor1_.x, this.anchor1_.y);
      if(d < 50){
-      if(this.pause_ == false){
-        this.pause_ = true;
-      }else{
-        this.pause_ = false;
+      this.pause_ = !this.pause_;
     }
   }
-}
 
   shapeDragged(){
     var d = dist(mouseX, mouseY, this.x_, this.y_);
@@ -248,7 +242,6 @@ class Wire{
   resume(){
     this.pause_ = false;
   }
-
 }
 
 function keyPressed(){
@@ -295,7 +288,7 @@ function mousePressed(){
   }
   if(scene == 2){
     for(const ball of upperBalls){
-      if(random() < 0.5){
+      if(random() < 0.9){
         ball.ballClicked();
       }
     }
@@ -303,10 +296,16 @@ function mousePressed(){
 }
 
 function mouseDragged(){
-  for(const wire of wires){
-    wire.setPos(mouseX/800);
+  if(scene == 1){
+    for(const wire of wires){
+      wire.setPos(mouseX/800);
+   }
+  }
+  if(scene == 2){
+    for(const ball of upperBalls){
+      if(random() < 0.3){
+        ball.ballClicked();
+      }
+    }
   }
 }
-
-
-
